c6b187d954131bdaae2ae634c8fd1923616c3259
Sanket  Singh <sanketsingh@Sankets-MacBook-Air.local>
2017-12-18 22:52:42 +0530

    Better way to bind local reference

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -9,18 +9,18 @@ export class CockpitComponent implements OnInit {
 
   @Output() serverCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
   @Output('bpCreated') blueprintCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
-  newServerContent = '';
+  @ViewChild('serverContentInput') serverContentInput: ElementRef;
   constructor() { }
 
   ngOnInit() {
   }
 
   onAddServer(serverNameInput: HTMLInputElement) {
-    this.serverCreated.emit({serverName: serverNameInput.value, serverContent: this.newServerContent});
+    this.serverCreated.emit({serverName: serverNameInput.value, serverContent: this.serverContentInput.nativeElement.value});
   }
 
   onAddBlueprint(serverNameInput: HTMLInputElement) {
-    this.blueprintCreated.emit({serverName: serverNameInput.value, serverContent: this.newServerContent});
+    this.blueprintCreated.emit({serverName: serverNameInput.value, serverContent: this.serverContentInput.nativeElement.value});
 
   }
 
